531071ca6ce61d8e9c6495be652128c994ed360e
zkr0023 <zkr0071@gmail.com>
2025-05-04 12:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,17 @@ import pandas as pd
 
 df = pd.read_csv('housing.csv')
 df
+
+X = housing.drop('median_house_value', axis=1)
+y = housing['median_house_value']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+col_names = ['total_rooms','total_bedrooms','population', 'households']
+rooms_id, bedrooms_id, population_id, households_id = [
+    housing.columns.get_loc(c) for c in col_names]
+print(rooms_id, bedrooms_id, population_id, households_id ) 
+
