340860dae0b824fb212bc4de89068b393d9bdd59
zkr0023 <zkr0071@gmail.com>
2025-05-04 12:51:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,57 @@ X_train, X_test, y_train, y_test = train_test_split(
 col_names = ['total_rooms','total_bedrooms','population', 'households']
 rooms_id, bedrooms_id, population_id, households_id = [
     housing.columns.get_loc(c) for c in col_names]
-print(rooms_id, bedrooms_id, population_id, households_id ) 
+# print(rooms_id, bedrooms_id, population_id, households_id ) 
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y=None):
+
+        return self
+
+    def transform(self,X):
+        rooms_per_household = X[:, rooms_id] / X[:, households_id]
+        population_per_household = X[:, population_id] / X[:, households_id]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_id] / X[:, rooms_id]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+num_attr = X.select_dtypes(include=np.number).columns.to_list()
+cat_attr = housing.select_dtypes(exclude=np.number).columns.to_list()
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler())
+])
+
+
+from sklearn.preprocessing import OneHotEncoder
+
+
+cat_pipeline = Pipeline([
+    ("onehot", OneHotEncoder())
+])
+
+
+from sklearn.compose import ColumnTransformer
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attr),
+     ("cat", cat_pipeline, cat_attr),
+])
+
+full_pipeline.fit(X_train)
+
 
